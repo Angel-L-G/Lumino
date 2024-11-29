2385bb103d8cd5f61056da83bd73f1db9b3b5a4b
moisesALH <moiluher@gmail.com>
2024-11-29 17:14:18 +0000

    refactor: refactoring form to work wihtout the model Enrollment

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from subjects.models import Subject
+
 from .models import Enrollment, Profile
 
 
@@ -13,21 +15,16 @@ class EditProfileForm(forms.ModelForm):
         }
 
 
-class EnrollSubjectsForm(forms.ModelForm):
-    class Meta:
-        model = Enrollment
-        fields = ['subject']
-        widgets = {
-            'subject': forms.CheckboxSelectMultiple,
-        }
+class EnrollSubjectsForm(forms.Form):
+    subjects = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.user = user
 
     def save(self, *args, **kwargs):
-        enrollment = super().save(commit=False)
-        enrollment.student = self.user
-        enrollment.subject = self.cleaned_data['subject']
-        enrollment.save()
-        return enrollment
+        subjects = self.cleaned_data['subjects']
+        for subject in subjects:
+            Enrollment.objects.create(student=self.user, subject=subject)
